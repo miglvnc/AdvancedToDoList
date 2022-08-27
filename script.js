84c78c8c52ee8ae188fc22af3e57abe8ef73f32c
miglvnc <mbogucki15@gmail.com>
2022-08-28 01:07:08 +0200

    added new funcs, styles, structure

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const mainFunction = () => {
     prepareDOMEvents();
     
     dateTimePicker();
+
 }
 
 function prepareDOMElements(){
@@ -15,23 +16,46 @@ function prepareDOMElements(){
     $listContainer = document.querySelector('.listContainer');
     $defaultTask = $listContainer.querySelector('.taskContainer');
     $ulList = $listContainer.querySelector('ul');
-    // let $idNumber = 0;
+
+    $completeBtn = document.querySelector('.complete')
+
+    let allTasks;
+    let allCompleteBtns;
+    let allEditBtns;
+    let allRemoveBtns;
+
+    let currentTask;
+    let currentDate;
+
+    let timer;
+
+    popUpTools = document.querySelector('.popUpEditTask');
+    popUpEditSmallDev = document.querySelector('.popUpTools');
+    
 }
 
 function prepareDOMEvents(){
-    $addNewTask.addEventListener('click', addNewTask)
+    $addNewTask.addEventListener('click', tasksManipulate)
     $toDoInput.addEventListener('keyup', enterConfirm)
+
+    // $ulList.addEventListener('click', checkToolsClick)
 }
 
+function tasksManipulate () {
+    addNewTask();
+}
+
+
 function addNewTask () {
-    if ($toDoInput.value !== "") {
+    if ($toDoInput.value !== "" && $dateInput.value !== "") {
+        $alertInfo.style.display = "none"
         $idNumber++;
         $newTaskContainer = $defaultTask.cloneNode(true);
         $newTaskContainer.setAttribute('id', `task-${$idNumber}`)
 
         //name
         let name = $newTaskContainer.querySelector('.task');
-        name.innerText = $toDoInput.value;
+        name.value = $toDoInput.value;
 
         //deadline
         let date = $newTaskContainer.querySelector('.deadline');
@@ -41,62 +65,157 @@ function addNewTask () {
         date.innerText = dateString.slice(0, dateString.length-7);
 
         //time remaining
-        timeCounter();
+        timeCounter($newTaskContainer, $dateInput.value);
 
+        //add task to list
         $ulList.appendChild($newTaskContainer);
 
+        //clear input
         $toDoInput.value = "";
         $alertInfo.innerText = "";
 
+        // allTasks = $ulList.querySelectorAll('li');
+        completeBtn = $newTaskContainer.querySelector('.complete');
+        editBtn = $newTaskContainer.querySelector('.edit');
+        removeBtn = $newTaskContainer.querySelector('.remove');
+
+        dateTimePicker();
+
+        deleteForm = $newTaskContainer.querySelectorAll('.dateInput')
+
+        // dateTimePicker duplicate inputs, so 2 of them is removed.
+        deleteForm[2].remove();
+        deleteForm[3].remove();
+        
+
+        
+        completeBtn.addEventListener('click', function(){
+
+            getThisContainers(this);
+
+            taskContainer.classList.toggle('taskDone');
+            taskContainer.querySelector('.timeleft').classList.toggle('taskDoneDate');
+            taskContainer.querySelector('.date').classList.toggle('dateTaskDone');
+
+            let allBtns = tools.children;
+
+            for (i = 0; i < allBtns.length; i++) {
+                allBtns[i].classList.toggle('btnBackground')
+            }
+            
+
+            if (taskContainer.querySelector('.timeleft').innerHTML !== "Done!") {
+                taskContainer.querySelector('.timeleft').innerHTML = "Done!";
+            } else {
+                taskContainer.querySelector('.timeleft').innerHTML = "Time left";
+            }
+        })
+
+        removeBtn.addEventListener('click', function(){
+
+            getThisContainers(this);
+            taskContainer.remove();
+        })
+
+        editBtn.addEventListener('click', function(){
+
+            getThisContainers(this);
+
+            if(this.innerHTML == "edit") {
+                this.innerHTML = 'save'
+                name.removeAttribute('readonly');
+                name.classList.add('editActive');
+                name.focus();
+
+                taskContainer.querySelectorAll('.dateInput')[0].classList.remove('hideDateInput');
+                taskContainer.querySelectorAll('.dateInput')[1].classList.remove('hideDateInput');
+                taskContainer.querySelector('.deadline').classList.add('hideDeadline');       
+                
+                tools.querySelector('.complete').classList.add('btnHide');
+                
+            } else {
+                name.setAttribute('readonly', 'readonly');
+                this.innerHTML = 'edit';
+                name.classList.remove('editActive');
+                
+                this.parentElement.querySelector('.complete').classList.remove('btnHide');
+                
+                taskContainer.querySelectorAll('.dateInput')[0].classList.add('hideDateInput');
+                taskContainer.querySelectorAll('.dateInput')[1].classList.add('hideDateInput');
+                taskContainer.querySelector('.deadline').classList.remove('hideDeadline');
+                
+                // console.log(this.parentElement.parentElement.parentElement.querySelectorAll('.dateInput'));
+                if(taskContainer.querySelectorAll('.dateInput')[0].value !== ""){
+                    const newDate = new Date(taskContainer.querySelectorAll('.dateInput')[0].value);
+                    let dateString = newDate.toUTCString();
+                    
+                    taskContainer.querySelector('.deadline').innerHTML = dateString.slice(0, dateString.length-7);
+
+                    clearInterval(timer)
+                    timeCounter(taskContainer, taskContainer.querySelectorAll('.dateInput')[0].value);
+                }
+            }
+        })
+        
     } else {
-        $alertInfo.innerText = "Type task name!"
+        $alertInfo.innerText = "Type task name and pick deadline!"
+    }
+    
+}
+
+function timeCounter(thisTimeContainer, dateInput) {
+
+    let timeleftContainer = thisTimeContainer.querySelector('.timeleft');
+    let endDate = new Date(dateInput).getTime();
+
+    console.log(timeleftContainer);
+    
+    timer = setInterval(function() {
+        let now = new Date().getTime();
+        let remaining = endDate - now;
+    
+        if (remaining >= 0 && timeleftContainer.innerHTML !== "Done!") {
+            let days = Math.floor(remaining / (1000*60*60*24));
+            let hours = Math.floor((remaining % (1000*60*60*24)) / (1000*60*60));
+            let mins = Math.floor(((remaining % (1000 * 60 * 60)) / (1000 * 60)));
+            let secs = Math.floor((remaining % (1000 * 60)) / 1000);
+
+            timeCounterConditions(timeleftContainer, days, hours, mins, secs);
+                    
+        } else if (remaining <= 0 && timeleftContainer.innerHTML !== "Done!") {
+            timeleftContainer.innerHTML = "Exceeded!"
+            timeleftContainer.classList.add('exceeded')
+            timeleftContainer.classList.remove('hourLeft')
+        }
+            
+    }, 1000)
+};
+ 
+function timeCounterConditions (timeleftContainer, days, hours, mins, secs) {
+    if (days+hours+mins === 0) {
+        timeleftContainer.innerHTML = ("0" + secs).slice(-2) + " seconds!"
+    } else if (days+hours === 0) {
+        timeleftContainer.innerHTML = ("0" + mins).slice(-2) + "m, " + ("0" + secs).slice(-2) + "s"
+    } else if (days === 0) {
+        timeleftContainer.innerHTML = ("0" + hours).slice(-2) + "h, " + ("0" + mins).slice(-2) + "m"
+    } else {
+        timeleftContainer.innerHTML = days + "d, " + ("0" + hours).slice(-2) + "h, " + ("0" + mins).slice(-2) + "m"
+    }
+
+    if(days === 0 && hours < 1) {
+        timeleftContainer.classList.add('hourLeft')
     }
 }
 
-        function timeCounter() {
-            let timeleftContainer = $newTaskContainer.querySelector('.timeleft');
-            let endDate = new Date($dateInput.value).getTime();
-    
-            console.log($dateInput.value);
-    
-            let timer = setInterval(function() {
-                let now = new Date().getTime();
-                let remaining = endDate - now;
-    
-                if (remaining >= 0) {
-                    let days = Math.floor(remaining / (1000*60*60*24));
-                    let hours = Math.floor((remaining % (1000*60*60*24)) / (1000*60*60));
-                    let mins = Math.floor(((remaining % (1000 * 60 * 60)) / (1000 * 60)));
-                    let secs = Math.floor((remaining % (1000 * 60)) / 1000);
-    
-                    console.log(days, hours, mins);
-    
-                    if (days+hours+mins === 0) {
-                        timeleftContainer.innerHTML = ("0" + secs).slice(-2) + "seconds!"
-                    } else if (days+hours === 0) {
-                        timeleftContainer.innerHTML = ("0" + mins).slice(-2) + "m, " + ("0" + secs).slice(-2) + "s"
-                    } else if (days === 0) {
-                        timeleftContainer.innerHTML = ("0" + hours).slice(-2) + "h, " + ("0" + mins).slice(-2) + "m"
-                    } else {
-                        timeleftContainer.innerHTML = days + "d, " + ("0" + hours).slice(-2) + "h, " + ("0" + mins).slice(-2) + "m"
-                    }
-                }
-            
-    
-            }, 1000)
-        }
-
-
-function taskModify (newTaskContainer) {
-    // let name = $listContainer
-}
-
-
 function enterConfirm() {
-    if (event.keyCode === 13) {
+    if (event.key === "Enter") {
         addNewTask();
     }
 }
+
+// sort
+
+//filter
 
 // date time picker
 const dateTimePicker = () => {
@@ -108,9 +227,34 @@ const dateTimePicker = () => {
         minDate: "today",
     }
     
-    flatpickr("#datetime", dateTimeConfig
+    flatpickr(".dateInput", dateTimeConfig
     );
+    // flatpickr(addNewTask, dateTimeConfig
+    // );
+}
+const dateTimePickerEdit = () => {
+    dateTimeConfig = {
+        enableTime: true,
+        altInput: true,
+        time_24hr: true,
+        altFormat: "F j, Y (H:i)",
+        minDate: "today",
+    }
+    
+    flatpickr(".dateInput", dateTimeConfig
+    );
+    // flatpickr(addNewTask, dateTimeConfig
+    // );
+}
+
+function getThisContainers(el){
+    tools = el.parentElement;
+    taskWrapper = tools.parentElement;
+    taskContainer = taskWrapper.parentElement
 }
 
 document.addEventListener("DOMContentLoaded", mainFunction)
+
+
+
 
